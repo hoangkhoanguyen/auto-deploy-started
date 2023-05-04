039b9558699c052138071e167df87a7461b41502
“hoangkhoanguyen” <anhhaido28@gmail.com>
2023-05-04 12:33:55 +0700

    11

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ router.use(function (req, res, next) {
 
 router.get('/', function (req, res) {
   // res.sendFile(path + 'index.html');
-  res.send('helloo baby');
+  res.send('helloo baby milo');
 });
 
 router.get('/sharks', function (req, res) {
