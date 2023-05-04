f76d38822450c4cde980db9c51c587d1f96fea98
“hoangkhoanguyen” <anhhaido28@gmail.com>
2023-05-04 20:03:29 +0700

    14

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ router.use(function (req, res, next) {
 
 router.get('/', function (req, res) {
   // res.sendFile(path + 'index.html');
-  res.send('helloo baby milo hihi');
+  res.send('helloo baby milo hihi haha');
 });
 
 router.get('/sharks', function (req, res) {
