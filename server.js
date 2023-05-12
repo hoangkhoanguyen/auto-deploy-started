e39f021cc698a8499fec3b5fc3eca094523733c5
khoanguyen-thegioiwhey <khoa.nguyen@thegioiwhey.com>
2023-05-12 10:56:38 +0700

    watch test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ router.use(function (req, res, next) {
 
 router.get('/', function (req, res) {
   // res.sendFile(path + 'index.html');
-  res.send('helloo baby milo hihi haha test server a Quý');
+  res.send('helloo baby milo hihi haha test server watcher');
 });
 
 router.get('/sharks', function (req, res) {
