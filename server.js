c74ba6e998d7903db10a4aab85c18ba405f36ccb
khoanguyen-thegioiwhey <khoa.nguyen@thegioiwhey.com>
2023-05-15 14:33:42 +0700

    env

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 
-const path = __dirname + '/views/';
+const path = __dirname + "/views/";
 const port = 3001;
 
 console.log(process.env.EXTERNAL_PORT);
 
 router.use(function (req, res, next) {
-  console.log('/' + req.method);
+  console.log("/" + req.method);
   next();
 });
 
-router.get('/', function (req, res) {
+router.get("/", function (req, res) {
   // res.sendFile(path + 'index.html');
-  res.send('helloo baby milo hihi haha test server watcher v1.5 ',process.env.EXTERNAL_PORT);
+  res.send(
+    "helloo baby milo hihi haha test server watcher v1.5 " +
+      process.env.EXTERNAL_PORT
+  );
 });
 
-router.get('/sharks', function (req, res) {
-  res.sendFile(path + 'sharks.html');
+router.get("/sharks", function (req, res) {
+  res.sendFile(path + "sharks.html");
 });
 
 app.use(express.static(path));
-app.use('/', router);
+app.use("/", router);
 
 app.listen(port, function () {
-  console.log('Example app listening on port: ', port);
+  console.log("Example app listening on port: ", port);
 });
